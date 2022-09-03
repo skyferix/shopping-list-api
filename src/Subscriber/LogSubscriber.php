1c9834d456a0e2403696244324177179037e0b54
Patrycjuš August Sinkevič <patrycjus-august.sinkevic@stud.vilniustech.lt>
2022-09-03 11:27:59 +0300

    parcial log subscriber

---
+++ src/Subscriber/LogSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LogSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+            KernelEvents::RESPONSE => 'onKernelResponse'
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $test = 1;
+    }
+
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $test = 1;
+    }
+}
